15ac3fd27a8e34786f4d5dd7ea93394f383ca538
HITEN900 <mohantyhiten579@gmail.com>
2026-01-28 18:45:50 +0530

    Fixed mobile menu and login/signup buttons for mobile

---
+++ otp.js
@@ -0,0 +1,14 @@
+// function verifyOtp() {
+//     const enteredOtp = document.getElementById("otpInput").value;
+//     const savedOtp = localStorage.getItem("generatedOtp");
+
+//     if (enteredOtp === savedOtp) {
+//         alert("✅ Account created successfully!");
+//         localStorage.removeItem("generatedOtp");
+
+//         // Redirect to home page
+//         window.location.href = "index.html"; // your home page
+//     } else {
+//         alert("❌ Invalid OTP. Try again");
+//     }
+// }
